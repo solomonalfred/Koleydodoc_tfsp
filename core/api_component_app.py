a81744151f39a0a2fa91cc910758c306eab7491b
solomonalfred <nik_bogdanov2002@mail.ru>
2023-12-06 18:09:09 +0500

    fix port

--- core/api_component_app.py
+++ core/api_component_app.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     config = Config(
         app=app,
         host="0.0.0.0",
-        port=7777,
+        port=8000,
         reload=True
     )
     server = Server(config)
