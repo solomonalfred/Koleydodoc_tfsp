c20b5d13f717ee5768dbc79cf95fa40b6961f342
solomonalfred <nik_bogdanov2002@mail.ru>
2023-12-06 17:55:10 +0500

    fix lang

--- core/api_component_app.py
+++ core/api_component_app.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     config = Config(
         app=app,
         host="0.0.0.0",
-        port=8000,
+        port=7777,
         reload=True
     )
     server = Server(config)
